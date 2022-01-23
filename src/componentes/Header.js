514b0087a8e4882de39e0e1f42e07b2c74b8bbb6
Julian Martinez <julian.fmartinez@gmail.com>
2022-01-23 18:14:49 -0300

    Se agrega titulo y formulario al index

---
+++ src/componentes/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'; //imr
+
+// function Header(props){
+    
+//     console.log(props)
+    
+//     return(
+//         <h1>{props.titulo}</h1>
+//     )
+// }
+
+const Header = (props) => <h1>{props.titulo}</h1>
+ 
+export default Header;
+
+// export default Header
